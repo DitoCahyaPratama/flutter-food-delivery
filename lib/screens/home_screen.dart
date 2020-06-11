13910a870f967324fcc54ce92270ff9970f45947
DitoCahyaPratama <ditocahyapratama717@gmail.com>
2020-06-11 19:08:21 +0700

    Create Recent Order

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,56 +11,58 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.account_circle),
-            iconSize: 30.0,
-            onPressed: () {},
-          ),
-          title: Text('Food Delivery'),
-          actions: <Widget>[
-            FlatButton(
-              child: Text(
-                'Cart (${currentUser.cart.length})',
-                style: TextStyle(color: Colors.white, fontSize: 20.0),
-              ),
-              onPressed: () {},
-            )
-          ],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
         ),
-        body: ListView(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: BorderSide(width: 0.8),
+        title: Text('Food Delivery'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                    width: 0.8,
+                    color: Theme.of(context).primaryColor,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: BorderSide(
-                      width: 0.8,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  hintText: 'Search Food or Restaurants',
-                  prefixIcon: Icon(
-                    Icons.search,
-                    size: 30.0,
-                  ),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.clear),
-                    onPressed: () {},
-                  ),
+                ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Icon(
+                  Icons.search,
+                  size: 30.0,
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.clear),
+                  onPressed: () {},
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+          RecentOrders(),
+        ],
+      ),
+    );
   }
 }
